0285b1353bd7e7b4a928acf2441919138ca73009
Abhinav Gupta <your-email-for-abgneudev@example.com>
2025-09-26 16:15:15 -0400

    added decorators for about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,18 +9,47 @@ import Timeline from '../components/Timeline'
 import CTASection from '../components/CTASection'
 import ScrollIndicator from '../components/ScrollIndicator'
 import AboutReview from '../components/AboutReview'
+import Decorators from '../components/common/Decorators'
+import ParallaxSection from '../components/ParallaxSection'
 
 const About: React.FC = () => {
   return (
-    <div className="about-page">
+    <div className="parallax-container">
       <Header />
       <AboutHero />
-      <AboutStory />
-      <CoreValues />
-      <TeamSection />
-      <Timeline />
-      <AboutReview />
-      <CTASection />
+      <ParallaxSection speed={0.1} direction="up">
+        <div style={{ position: 'relative' }}>
+          <AboutStory />
+          <Decorators count={2} seed={5} />
+        </div>
+      </ParallaxSection>
+      <ParallaxSection speed={0.15} direction="up">
+        <div style={{ position: 'relative' }}>
+          <CoreValues />
+          <Decorators count={2} seed={4} />
+        </div>
+      </ParallaxSection>
+      <ParallaxSection speed={0.08} direction="up">
+        <div style={{ position: 'relative' }}>
+          <TeamSection />
+          <Decorators count={2} seed={2} />
+        </div>
+      </ParallaxSection>
+      <ParallaxSection speed={0.12} direction="up">
+        <Timeline />
+      </ParallaxSection>
+      <ParallaxSection speed={0.1} direction="up">
+        <div style={{ position: 'relative' }}>
+          <AboutReview />
+          <Decorators count={2} seed={7} />
+        </div>
+      </ParallaxSection>
+      <ParallaxSection speed={0.08} direction="up">
+        <div style={{ position: 'relative' }}>
+          <CTASection />
+          <Decorators count={2} seed={7} />
+        </div>
+      </ParallaxSection>
       <Footer />
       <ScrollIndicator />
     </div>
