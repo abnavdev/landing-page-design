7653a0acb8ebde022d115f0c114197f422f2170a
Abhinav Gupta <your-email-for-abgneudev@example.com>
2025-09-26 16:28:07 -0400

    added decorators for tutorlist

--- src/pages/Tutors.tsx
+++ src/pages/Tutors.tsx
@@ -6,16 +6,28 @@ import TutorHero from '../components/TutorHero'
 import TutorList from '../components/TutorList'
 import CTASection from '../components/CTASection'
 import ScrollIndicator from '../components/ScrollIndicator'
+import ParallaxSection from '../components/ParallaxSection'
+import Decorators from '../components/common/Decorators'
 
 const Tutors: React.FC = () => {
   return (
     <div className="tutors-page">
       <Header />
-      <TutorHero />
+      <ParallaxSection speed={0.08} direction="up">
+        <div style={{ position: 'relative' }}>
+          <TutorHero />
+          <Decorators count={2} seed={10} />
+        </div>
+      </ParallaxSection>
       <main className="section tutors-section">
         <TutorList />
       </main>
-      <CTASection />
+      <ParallaxSection speed={0.08} direction="up">
+        <div style={{ position: 'relative' }}>
+          <CTASection />
+          <Decorators count={2} seed={10} />
+        </div>
+      </ParallaxSection>
       <Footer />
       <ScrollIndicator />
     </div>
